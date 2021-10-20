9b511892fc61a0f31d7a860a85a55e65bf360488
Nicolas Mahnic <nmahnic@pdssa.com.ar>
2021-10-20 15:59:44 -0300

    leff(w)

--- ls_rs3.py
+++ ls_rs3.py
@@ -1,5 +1,6 @@
 import scipy.constants as cts
 from scipy.special import jv
+import cmath
 
 skinDepth = 5.31e-3
 cond = 14.54
@@ -8,10 +9,13 @@ ro = 12e-3
 f = 152e6
 w = 2*cts.pi*f
 
-k = (1/skinDepth)*(1-1j)
-# k = (1/skinDepth)-(w/cts.c)*1j
+# k = (1/skinDepth)*(1-1j)
+k = (1/skinDepth)-(w/cts.c)*1j
+# a = k*ro
+# a = cmath.polar(a)[0]
 a = (k*ro).real
 print("k={:.3e}".format(k))
+print("kro={:.3e}".format(k*ro))
 print("a={:.3e}".format(a))
 u = ((k**2)/(cond*w*-1j)).real
 
@@ -22,6 +26,10 @@ j1 = jv(1,a)
 print("Jo({:2.3f}".format(a)+")={:2.4e}".format(jo))
 print("J1({:2.3f}".format(a)+")={:2.4e}".format(j1))
 
-Zs = (a*jo)/(2*cts.pi*ro*cond*j1)
+rs = (a)/(2*cts.pi*ro**2*cond*j1)
 
-print("Zs={:.4e}".format(Zs))
+print("rs={:.4f}".format(rs))
+
+ls = (u*(1+jo))/(2*cts.pi*a*j1)
+
+print("ls={:.6e}".format(ls))
